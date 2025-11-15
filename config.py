488014371afa49f82e73a73c723c468e2e1b9cb9
RAHAT11790 <rahatsarker224@gmail.com>
2025-11-15 23:38:04 +0600

    Upload config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+# Bot token @Botfather
+BOT_TOKEN = "8443325052:AAGFagO16Fgxh1gBxsqui5n7EtJKa4WoMoI"
+
+# Your API ID from my.telegram.org
+API_ID = 25976192
+
+# Your API Hash from my.telegram.org
+API_HASH = "8ba23141980539b4896e5adbc4ffd2e2"
+
+# Your Owner / Admin Id For Broadcast 
+ADMINS = 6621572366
+
+# Database Name (SQLite will be used) - শুধু user session storage
+DB_NAME = "rssavecontentbot"
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
+ERROR_MESSAGE = True
+
+# Temporary download directory (will be deleted after upload)
