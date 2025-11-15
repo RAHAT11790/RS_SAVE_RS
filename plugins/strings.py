9f9d60db642f50e1ff60b0fe88b9454f8addff0c
RAHAT11790 <rahatsarker224@gmail.com>
2025-11-15 23:38:13 +0600

    Upload strings.py

---
+++ plugins/strings.py
@@ -0,0 +1,46 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+HELP_TXT = """**🌟 Help Menu** 
+
+**__FOR PRIVATE CHATS__**
+
+__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__
+
+
+**__FOR BOT CHATS__**
+
+__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
+
+```
+https://t.me/b/botusername/4321
+```
+
+**__MULTI POSTS__**
+
+__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+
+
+```
+https://t.me/xxxx/1001-1010
+
+https://t.me/c/xxxx/101 - 120
+```
+
+**Steps:**
+1. First use `/login` to login with your account
+2. Send the post link to download restricted content
+3. Use `/cancel` to stop any ongoing process
+
+**Note:** The bot will download content that your logged-in account has access to."""
+
+ABOUT_TXT = """**About This Bot:**
+
+🤖 **RS Save Restricted Content Bot**
+👨‍💻 **Developer:** @RS_WONER  
+📢 **Channel:** @cartoonfunny03  
+👥 **Support:** @cartoonfunny03  
+
+This bot helps you download restricted content from Telegram using your own account."""
